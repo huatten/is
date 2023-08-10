f1251e852d896c0b58c92daf0cd02336e72773e5
huatten <nbilly@163.com>
2023-08-10 11:43:58 +0800

    build: 🛠 在 jest 中支持 esm

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,38 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: 'usage',
-        shippedProposals: true,
-        corejs: {
-          version: '3.32',
-          proposals: true,
+module.exports = function (api) {
+  const testEnv = api.env('test') // Jest default environment variables is `test` @see https://jestjs.io/zh-Hans/docs/environment-variables
+  /**
+   * @description how to support ESM in Jest
+   * @see https://juejin.cn/post/6999572866815492110
+   * @type {{presets: [string,{targets: {node: string}}][]}}
+   */
+  const test = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: { node: 'current' },
         },
-      },
+      ],
     ],
-  ],
+  }
+
+  const dom = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          useBuiltIns: 'usage',
+          shippedProposals: true,
+          corejs: {
+            version: '3.32',
+            proposals: true,
+          },
+        },
+      ],
+    ],
+  }
+
+  let config = testEnv ? test : dom
+  return config
 }
--- __tests__/type/is-string.test.js
+++ __tests__/type/is-string.test.js
@@ -7,3 +7,14 @@ test('`123`是字符串', () => {
 test('123不是字符串', () => {
   expect(isString(123)).toBe(false)
 })
+
+// eslint-disable-next-line no-new-wrappers
+const newStr = new String(123)
+test('`new String(123)`是字符串', () => {
+  expect(isString(newStr)).toBe(true)
+})
+
+const str = String(123)
+test('`String(123)`是字符串', () => {
+  expect(isString(str)).toBe(true)
+})
