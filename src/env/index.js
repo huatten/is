d3a4ffbd17cec95b66e0479d8d5228df0f4e5d76
huatten <nbilly@163.com>
2023-08-21 22:25:50 +0800

    feat(env): 🚀 入口文件导出方法

--- src/env/index.js
+++ src/env/index.js
@@ -1,7 +1,59 @@
+import isAndroid from './is-android'
+import isAndroidPhone from './is-android-phone'
+import isAndroidTablet from './is-android-tablet'
+import isBlackberry from './is-blackberry'
+import isBlackberryPhone from './is-blackberry-phone'
+import isBlackberryTablet from './is-blackberry-tablet'
 import isBrowser from './is-browser'
+import isChrome from './is-chrome'
+import isDesktop from './is-desktop'
+import isEdge from './is-edge'
+import isFirefox from './is-firefox'
+import isIE from './is-ie'
+import isIos from './is-ios'
+import isIpad from './is-ipad'
+import isIphone from './is-iphone'
+import isIpod from './is-ipod'
 import isLinux from './is-linux'
 import isMac from './is-mac'
+import isMeego from './is-meego'
+import isMobile from './is-mobile'
 import isNode from './is-node'
+import isOpera from './is-opera'
+import isSafari from './is-safari'
+import isTablet from './is-tablet'
+import isWechat from './is-wechat'
 import isWindows from './is-windows'
+import isWindowsPhone from './is-windows-phone'
+import isWindowsTablet from './is-windows-tablet'
 
-export { isBrowser, isLinux, isMac, isNode, isWindows }
+export {
+  isAndroid,
+  isAndroidPhone,
+  isAndroidTablet,
+  isBlackberry,
+  isBlackberryPhone,
+  isBlackberryTablet,
+  isBrowser,
+  isChrome,
+  isDesktop,
+  isEdge,
+  isFirefox,
+  isIE,
+  isIos,
+  isIpad,
+  isIphone,
+  isIpod,
+  isLinux,
+  isMac,
+  isMeego,
+  isMobile,
+  isNode,
+  isOpera,
+  isSafari,
+  isTablet,
+  isWechat,
+  isWindows,
+  isWindowsPhone,
+  isWindowsTablet,
+}
